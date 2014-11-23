1e2564d77824ab31d3cdbff2120ff63e061947ad
rafaelvjb <rafaelvjb@hotmail.com>
2014-11-23 18:16:55 -0200

    included model and map. Error config entity

---
+++ CodeFirstMigrations/EF.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using EF.Data;
+using EF.Core;
+
+namespace EF.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter your name : ");
+            string name = System.Console.ReadLine();
+            System.Console.Write("Enter your age : ");
+            int age = 0;
+            Int32.TryParse(System.Console.ReadLine(), out age);
+
+            using (EFDbContext context = new EFDbContext())
+            {
+                Student student = new Student { Name = name, Age = age };
+                context.Entry(student).State = System.Data.Entity.EntityState.Added;
+                context.SaveChanges();
+            }
+            System.Console.ReadLine();   
+        }
+    }
+}
---
+++ CodeFirstMigrations/EF.Data/StudentMap.cs
@@ -0,0 +1,28 @@
+﻿using EF.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data
+{
+    public class StudentMap : EntityTypeConfiguration<Student>
+    {
+        public StudentMap()
+        {
+            //key  
+            HasKey(t => t.Id);
+
+            //property  
+            Property(t => t.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Name);
+            Property(t => t.Age);
+
+            //table  
+            ToTable("Student");  
+        }
+    }
+}
