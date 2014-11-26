33b53ed4491da14ca8383d38eca4a5497b461662
rafaelvjb <rafaelvjb@hotmail.com>
2014-11-26 00:01:54 -0200

    finished basic tuto about angular js - http://www.codeproject.com/Articles/832288/CRUD-with-SPA-ASP-NET-Web-API-and-Angular-js

---
+++ RDLCReport/RDLCReport/Controllers/CustomerController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace RDLCReport.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        InventoryEntities db = new InventoryEntities();
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return db.Customers.AsEnumerable();
+        }
+
+        public Customer Get(int id)
+        {
+            var customer = db.Customers.Find(id);
+
+            if (customer == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return customer;
+        }
+
+        public HttpResponseMessage Post(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, customer);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = customer.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != customer.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex )
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, customer);
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            var customer = db.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Customers.Remove(customer);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RDLCReport/RDLCReport/Scripts/app/customerCtrl.js
@@ -0,0 +1,101 @@
+﻿(function () {
+    'use strict';
+
+    //create angularjs controller
+    var app = angular.module('app', []);//set and get the angular module
+    app.controller('customerController', ['$scope', '$http', customerController]);
+
+    //angularController Method
+    function customerController($scope, $http) {
+        //declare variable for mainain ajax Load and entry or edit mode
+        $scope.loading = true;
+        $scope.addMode = false;
+
+        //get all customers information
+        $http.get('/api/Customer')
+            .success(function (data) {
+                $scope.customers = data;
+                $scope.loading = false;
+            })
+            .error(function () {
+                $scope.error = "An error occoured while loading posts!";
+                $scope.loading = false;
+            });
+
+        //by pressing toggleEdit button ng-click in html, this method will de hit
+        $scope.toggleEdit = function () {
+            this.customer.editMode = !this.customer.editMode;
+        };
+
+        //by pressing toggleAdd button ng-click in html, this method will be hit
+        $scope.toggleAdd = function () {
+            $scope.addMode = !$scope.addMode;
+        };
+
+        //insert Customer
+        $scope.add = function () {
+            $scope.loading = true;
+            $http.post('/api/Customer/', this.newcustomer)
+                .success(function (data) {
+                    alert("Added Successfully!");
+
+                    $scope.addMode = false;
+                    $scope.customers.push(data);
+                    $scope.loading = false;
+
+                })
+                .error(function (data) {
+                    $scope.error = "An Error occoured while insert a Customer! " + data;
+                    $scope.loading = false;
+                });
+        };
+
+        //Edit Customer
+        $scope.save = function () {
+            alert("Edit");
+
+            $scope.loading = true;
+
+            var frien = this.customer;
+            alert(frien);
+
+            $http.put('/api/Customer/' + frien.Id, frien)
+                .success(function (data) {
+                    alert("Saved Succefully!");
+
+                    frien.editMode = false;
+                    $scope.loading = false;
+                })
+                .error(function (data) {
+                    $scope.error = "An error occoured while edit Customer!" + data;
+                    scope.loading = false;
+                });
+        };
+
+        //Delete Customer
+        $scope.deleteCustomer = function () {
+            $scope.loading = true;
+
+            var Id = this.customer.Id;
+
+            $http.delete('/api/Customer/' + Id)
+                .success(function (data) {
+                    alert("Deleted Succefully");
+
+                    $.each($scope.customers, function (i) {
+                        if ($scope.customers[i].Id === Id) {
+                            $scope.customers[i].splice(i, 1);
+                            return false;
+                        }
+                    });
+                    $scope.loading = false;
+                })
+                .error(function (data) {
+                    $scope.error = "An error occoured while Deleting Customer! " + data;
+                    $scope.loading = false;
+                });
+        };
+
+    }
+
+})();
