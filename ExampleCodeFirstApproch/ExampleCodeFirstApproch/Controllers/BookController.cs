a011e0545ef37f2e68e2593e92a4d3ece3e376c6
rafaelvjb <rafaelvjb@hotmail.com>
2014-11-30 20:48:50 -0200

    finished step 3

---
+++ ExampleCodeFirstApproch/ExampleCodeFirstApproch/Controllers/BookController.cs
@@ -0,0 +1,86 @@
+﻿using ExampleCodeFirstApproch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleCodeFirstApproch.Controllers
+{
+    public class BookController : Controller
+    {
+        LibraryContext objContext;
+
+        public BookController()
+        {
+            objContext = new LibraryContext();
+        }
+
+        #region List and Details Block
+        public ActionResult Index()
+        {
+            var books = objContext.Book.ToList();
+
+            return View(books);
+        }
+
+        public ViewResult Details(int id)
+        {
+            Book book = objContext.Book.Where(x => x.BookId == id).SingleOrDefault();
+            return View(book);
+        }
+
+        #endregion
+        #region Create Publisher
+        public ActionResult Create()
+        {
+            return View(new Book());
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            objContext.Book.Add(book);
+            objContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+        #region Edit Book
+        public ActionResult Edit(int id)
+        {
+            Book book = objContext.Book.Where(x => x.BookId == id).SingleOrDefault();
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book model)
+        {
+            Book book = objContext.Book.Where(x => x.BookId == model.BookId).SingleOrDefault();
+            if (book != null)
+            {
+                objContext.Entry(book).CurrentValues.SetValues(model);
+                objContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        #endregion
+        #region Delete Book
+        public ActionResult Delete(int id)
+        {
+            Book book = objContext.Book.Find(id);
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Publisher model)
+        {
+            var book = objContext.Book.Where(x => x.BookId == id).SingleOrDefault();
+            if (book != null)
+            {
+                objContext.Book.Remove(book);
+                objContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+    }
+}
---
+++ ExampleCodeFirstApproch/ExampleCodeFirstApproch/Controllers/PublisherController.cs
@@ -0,0 +1,86 @@
+﻿using ExampleCodeFirstApproch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleCodeFirstApproch.Controllers
+{
+    public class PublisherController : Controller
+    {
+        LibraryContext objContext;
+        public PublisherController()
+        {
+            objContext = new LibraryContext();
+        }
+        #region List and Details Publisher
+        public ActionResult Index()
+        {
+            var publisher = objContext.Publisher.ToList();
+            return View(publisher);
+        }
+        public ViewResult Details(int id)
+        {
+            Publisher publisher =
+              objContext.Publisher.Where(x => x.PublisherId == id).SingleOrDefault();
+            return View(publisher);
+        }
+        #endregion
+        #region Create Publisher
+        public ActionResult Create()
+        {
+            return View(new Publisher());
+        }
+        [HttpPost]
+        public ActionResult Create(Publisher publish)
+        {
+            objContext.Publisher.Add(publish);
+            objContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+        #region edit publisher
+        public ActionResult Edit(int id)
+        {
+            Publisher publisher = objContext.Publisher.Where(
+              x => x.PublisherId == id).SingleOrDefault();
+            return View(publisher);
+        }
+        [HttpPost]
+        public ActionResult Edit(Publisher model)
+        {
+            Publisher publisher = objContext.Publisher.Where(
+              x => x.PublisherId == model.PublisherId).SingleOrDefault();
+            if (publisher != null)
+            {
+                objContext.Entry(publisher).CurrentValues.SetValues(model);
+                objContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        #endregion
+        #region Delete Publisher
+        public ActionResult Delete(int id)
+        {
+            Publisher publisher = objContext.Publisher.Find(id);
+            //.Where(x => x.PublisherId == id).SingleOrDefault();
+
+            return View(publisher);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Publisher model)
+        {
+            var publisher =
+              objContext.Publisher.Where(x => x.PublisherId == id).SingleOrDefault();
+            if (publisher != null)
+            {
+                objContext.Publisher.Remove(publisher);
+                objContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion 
+    }
+}
