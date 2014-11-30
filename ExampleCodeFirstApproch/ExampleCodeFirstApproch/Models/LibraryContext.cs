229169fb55d794650d583d3a7574426bb98df673
rafaelvjb <rafaelvjb@hotmail.com>
2014-11-30 16:11:49 -0200

    step 3 tutorial code project

---
+++ ExampleCodeFirstApproch/ExampleCodeFirstApproch/Models/LibraryContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExampleCodeFirstApproch.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext()
+            : base("name=DbConnectionSctring")
+        {
+        }
+
+        public DbSet<Publisher> Publisher { get; set; }
+        public DbSet<Book> Book { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Publisher>().HasKey(t => t.PublisherId);
+            modelBuilder.Entity<Publisher>().Property(t => t.PublisherId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Book>().HasKey(b => b.BookId);
+            modelBuilder.Entity<Book>().Property(b => b.BookId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<Book>().HasRequired(p => p.Publisher)
+                .WithMany(b => b.Books).HasForeignKey(b => b.PublisherId);      
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
